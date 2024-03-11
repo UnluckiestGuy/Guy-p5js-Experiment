14a64371d5fbfc656899f8454f0c76fe2719bbac
UnluckiestGuy <162921087+UnluckiestGuy@users.noreply.github.com>
2024-03-11 15:53:15 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ let gameOverBool = false;
 let strikes = 0;
 let score = 0;
 let playerX = 200;
+let playing = false;
 
 function setup() {
   createCanvas(400, 400);
@@ -34,144 +35,153 @@ function setup() {
 
 function draw() {
   background(50);
-  
-  // The ultimate bar
-  // red rectangle
-  push();
-  fill(255,100,100);
-  rectMode(CENTER);
-  rect(width/16,height/2,25,300);
-  // white rectangle
-  rectMode(CORNER);
-  fill(255);
-  rect(width/16-12.5,height/2-150,25,ultCharge);
-  pop();
-
-  if (kb.pressing("A")) {
-    playerX -= 5;
-  }
-  if (kb.pressing("D")) {
-    playerX += 5;
-  }
-  if (playerX < 0+10) {
-    playerX = 10;
-  }
-  if (playerX > width-10) {
-    playerX = width-10;
-  }
-  circle(playerX, height - 30, 25);
-
-  // draws lasers
-  for (let laser of lasers) {
-    laser.y -= 5;
-    circle(laser.x, laser.y, 10);
-  }
-
-  // draws aliens
-  for (let alien of aliens) {
-    alien.y = alien.y + 3;
-    square(alien.x, alien.y, 30);
-  }
-
-  // deals with killing aliens
-  for (let alien of aliens) {
-    for (let laser of lasers) {
-      if (dist(alien.x, alien.y, laser.x, laser.y) < 30) {
-        aliens.splice(aliens.indexOf(alien), 1);
-        lasers.splice(lasers.indexOf(laser), 1);
-        let newAlien = {
-          x: random(50, width-50),
-          y: random(-500, -35),
-        };
-        aliens.push(newAlien);
-        score++;
-        if (ultCharge > 0 && ulting == false) {
-        ultCharge -= 20;
-        }
-      }
-    }
-  }
-  
-  // Every 10 seconds, add one alien to the total
-  if (gameOverBool == false) {
-    if (frameCount - pFrameCount >= 600) {
-      let alien = {
-        x: random(100,width-50),
-        y: random(-1000, -35),
-      };
-      aliens.push(alien);
-      pFrameCount = frameCount;
-    }
-  }
-  
-  // If an alien goes past the bottom of the screen, send it back to the top and add a strike
-  for (let alien of aliens) {
-    if (alien.y > height+50) {
-      alien.y = random(-500, -35);
-      strikes++;
-    }
-  }
-  
-  // displays the score
-  push();
-  textAlign(CENTER);
-  textFont("impact",30);
-  fill(255);
-  text(str(score),width-20,30)
-  pop();
-  
-  // displays the strikes
-  push();
-  stroke(255,0,0);
-  strokeWeight(5);
-  for (let i = 0; i < strikes; i++) {
-    line(16,16,36,36);
-    line(16,36,36,16);
-    translate(30,0);
-  }
-  pop();
-  
-  // if there are 3 strikes, game over
-  if (strikes >= 3) {
-    gameOver();
-  }
-  
-  // the gameover text
-  if (gameOverBool == true) {
+  if (playing == false) {
     push();
     textAlign(CENTER);
     textFont("impact",40);
     fill(255);
-    text("GAME OVER",width/2,height/2);
+    text("START?",width/2,height/2);
     pop();
   }
+  if (playing == true) {
+    // The ultimate bar
+    // red rectangle
+    push();
+    fill(255,100,100);
+    rectMode(CENTER);
+    rect(width/16,height/2,25,300);
+    // white rectangle
+    rectMode(CORNER);
+    fill(255);
+    rect(width/16-12.5,height/2-150,25,ultCharge);
+    pop();
   
-  // if the laser goes past the top of the screen, delete it
-  for (let laser of lasers) {
-    if (laser.y<-10) {
-      lasers.splice(lasers.indexOf(laser), 1);
+    if (kb.pressing("A")) {
+      playerX -= 5;
     }
-  }
+    if (kb.pressing("D")) {
+      playerX += 5;
+    }
+    if (playerX < 0+10) {
+      playerX = 10;
+    }
+    if (playerX > width-10) {
+      playerX = width-10;
+    }
+    circle(playerX, height - 30, 25);
   
-  // the ult
-  if (ulting) {
-    if (cooldown == 5) {
-     let lLaser = {
-      x: playerX-10,
-      y: height-40,
-    };
-      let rLaser = {
-      x: playerX+10,
-      y: height-40,
-    };
-      lasers.push(lLaser,rLaser);
-      cooldown = 0;
+    // draws lasers
+    for (let laser of lasers) {
+      laser.y -= 5;
+      circle(laser.x, laser.y, 10);
     }
-    cooldown++;
-    ultCharge+=2;
-  }
-  if (ultCharge == 300) {
-    ulting = false;
+  
+    // draws aliens
+    for (let alien of aliens) {
+      alien.y = alien.y + 3;
+      square(alien.x, alien.y, 30);
+      }
+  
+    // deals with killing aliens
+    for (let alien of aliens) {
+      for (let laser of lasers) {
+        if (dist(alien.x, alien.y, laser.x, laser.y) < 30) {
+          aliens.splice(aliens.indexOf(alien), 1);
+          lasers.splice(lasers.indexOf(laser), 1);
+          let newAlien = {
+            x: random(50, width-50),
+            y: random(-500, -35),
+          };
+          aliens.push(newAlien);
+          score++;
+          if (ultCharge > 0 && ulting == false) {
+          ultCharge -= 20;
+          }
+        }
+      }
+    }
+
+    // Every 10 seconds, add one alien to the total
+    if (gameOverBool == false) {
+      if (frameCount - pFrameCount >= 600) {
+        let alien = {
+          x: random(100,width-50),
+          y: random(-1000, -35),
+        };
+        aliens.push(alien);
+        pFrameCount = frameCount;
+      }
+    }
+
+    // If an alien goes past the bottom of the screen, send it back to the top and add a strike
+    for (let alien of aliens) {
+      if (alien.y > height+50) {
+        alien.y = random(-500, -35);
+        strikes++;
+      }
+    }
+
+    // displays the score
+    push();
+    textAlign(CENTER);
+    textFont("impact",30);
+    fill(255);
+    text(str(score),width-20,30)
+    pop();
+
+    // displays the strikes
+    push();
+    stroke(255,0,0);
+    strokeWeight(5);
+    for (let i = 0; i < strikes; i++) {
+      line(16,16,36,36);
+      line(16,36,36,16);
+      translate(30,0);
+    }
+    pop();
+
+    // if there are 3 strikes, game over
+    if (strikes >= 3) {
+      gameOver();
+    }
+
+    // the gameover text
+    if (gameOverBool == true) {
+      push();
+      textAlign(CENTER);
+      textFont("impact",40);
+      fill(255);
+      text("GAME OVER",width/2,height/2);
+      pop();
+    }
+
+    // if the laser goes past the top of the screen, delete it
+    for (let laser of lasers) {
+      if (laser.y<-10) {
+        lasers.splice(lasers.indexOf(laser), 1);
+      }
+    }
+
+    // the ult
+    if (ulting) {
+      if (cooldown == 5) {
+       let lLaser = {
+        x: playerX-10,
+        y: height-40,
+      };
+        let rLaser = {
+        x: playerX+10,
+        y: height-40,
+      };
+        lasers.push(lLaser,rLaser);
+        cooldown = 0;
+      }
+      cooldown++;
+      ultCharge+=2;
+    }
+    if (ultCharge == 300) {
+      ulting = false;
+    }
   }
 }
 
@@ -197,8 +207,10 @@ function keyPressed() {
   }
 }
 
-//spawn lasers when mouse is clicked
 function mousePressed() {
+  if (playing == false) {
+    playing = true;
+  }
 }
 
 function gameOver() {
