ae25d70e07ed7abc846cab96575dfd7783c357f3
UnluckiestGuy <162921087+UnluckiestGuy@users.noreply.github.com>
2024-08-20 15:38:43 -0500

    Update sketch.js
    removed sounds again

--- sketch.js
+++ sketch.js
@@ -167,7 +167,7 @@ function draw() {
           // if the laser and the alien in question are overlapping
           alien.life = 0; // alien begone
           if (sounded == false) {
-            alienDeathSound.start();
+            //alienDeathSound.start();
             sounded = true;
           }
           // take the alien and laser out of their respective arrays
@@ -281,7 +281,7 @@ function draw() {
           lasers.push(laser);
           // prevents the laser sound from being called twice in the same frame (breaks the game)
           if (sounded == false) {
-            laserSound.start();
+            //laserSound.start();
             sounded = true;
           }
         }
@@ -308,7 +308,7 @@ function draw() {
           };
           // prevents the laser sound from being called twice in the same frame (breaks the game)
           if (sounded == false) {
-            laserSound.start();
+            //laserSound.start();
             sounded = true;
           }
           lasers.push(lLaser, rLaser); // add these lasers to the "lasers" array
@@ -409,7 +409,7 @@ function gameOver() {
 
   // play the death sound and replace the player's image with the explosion animation
   if (sounded == false) {
-    playerDeathSound.start();
+    //playerDeathSound.start();
     sounded = true;
   }
   kaboom(spaceshipSprite);
